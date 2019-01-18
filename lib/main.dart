c794724bc6cbc91453b5610f6dd803b94228ab68
Chamuel Castillo <cjimenez@ntdingredientes.com>
2019-01-18 17:06:16 -0400

    MODIFIED: json method

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,54 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
-void main (){
+void main () async{
 
+  List _data = await getJson();
+
+  print(_data[0]);
   runApp(new MaterialApp(
-    home:new Scaffold(
-      appBar: new AppBar(
+      home:new Scaffold(
+        appBar: new AppBar(
 
-        title: new Text('Quake-app'),
-        centerTitle: true,
-        backgroundColor: Colors.greenAccent,
+          title: new Text('Quake-app'),
+          centerTitle: true,
+       backgroundColor: Colors.greenAccent,
 
 
-      ),
-      body: new Center(
-        
-        child: new ListView.builder(itemBuilder: null),
-      ),
-    )
+        ),
+        body: new Center(
+
+//        child: new ListView.builder(
+//            itemCount: _data.length * 2,
+//            padding: const EdgeInsets.all(16.0),
+////            itemBuilder: (BuildContext context, int position ){
+////              if(position.isOdd) return new Divider();
+////
+////              final index = position ~/ 2;
+////
+////              return new ListTile(
+////
+////                title: new Text(
+////                    "hello people"
+////                ),
+////              );
+////            }
+//
+//
+//
+//        ),
+        ),
+      )
   ));
 
 
 }
 
+Future<List> getJson() async{
+
+  String apiUrl = '[https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson]';
+
+  http.Response response = await http.get(apiUrl);
+
+  return json.decode(response.body);
+}
